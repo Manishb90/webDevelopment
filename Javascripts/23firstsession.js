bdefb444594650e478e4250c07d23475c7d8f3ae
Manishb90 <Manishb90@hotmail.com>
2022-08-23 23:05:43 -0500

    23 work

---
+++ Javascripts/23firstsession.js
@@ -0,0 +1,42 @@
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(arr[i]);
+
+// }
+
+// for (number of arr) {
+//     console.log(number);
+// }
+
+// for (i in arr) {
+//     console.log(i);
+// }
+// let i = 0;
+// while (i > 8) {
+//     console.log("Running");
+//     i++;
+// }
+
+//foreach loop
+
+// function display(a) {
+//     a(10);
+// }
+// display(function display(number) {
+//     console.log(number)
+// })
+const arr = [11, 12, 13, 14, 15, 16, 17];
+// console.log(arr);
+arr.forEach((item, index, array) => console.log(item));
+// function a() 
+    // console.log("running");
+
+
+
+
+// function add(a, b) {
+//     return a + b;
+// }
+// const result = add(10, 20);
+// console.log(result);
